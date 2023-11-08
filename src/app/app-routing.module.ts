50e87ed746e96b154e9fecfbe2447c5233b91d3d
akpalo <akseli.palo@student.lab.fi>
2023-11-09 00:20:33 +0200

    9.11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { KomponenttiComponent } from './komponentti/komponentti.component';
+import { LaskinComponent } from './laskin/laskin.component';
 
 const routes: Routes = [
-  {path:'komponentti', component: KomponenttiComponent}];
+  {path:'komponentti', component: KomponenttiComponent},
+  {path:'laskin', component: LaskinComponent},
+  {path: '', redirectTo: '/laskin', pathMatch: 'full'}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/laskin/laskin.component.ts
+++ src/app/laskin/laskin.component.ts
@@ -3,12 +3,51 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-laskin',
   templateUrl: './laskin.component.html',
-  styleUrls: ['./laskin.component.css']
+  styleUrls: ['./laskin.component.css'],
 })
 export class LaskinComponent {
-      buttons: string[] = [
-        '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', 'C', '0', '+', '='
-      ];
-}
+  buttons: string[] = [
+    '7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', 'C', '0', '+', '='
+  ];
+
+  laskinTitle: string;
+
+  constructor() {
+    this.laskinTitle = 'Nelilaskin';
+  }
+
+  result: any;
+
+  calculate(value: any) {
+    console.log('addValue value: ' + value)
+
+    if (value === 'C') {
+      this.result = '';
+    } else if (value === '=') {
+      try {
+        this.result = eval(this.result);
+      } catch (error) {
+        this.result = 'Error';
+      }
+    } else {
+      if (this.result === 'Error') {
+        this.result = '';
+      }
+      if (value.match(/[+\-*/]/) && this.result[this.result.length - 1].match(/[+\-*/]/)) {
+        // Varmista, ettei lisätä kahta operaattoria peräkkäin
+        return;
+      }
+      this.result += value;
+    }
+
+    }
+  }
+
+
+
+
+
+
+
 
 
